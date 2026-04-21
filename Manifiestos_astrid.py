ffee8c48880ea6546fa9ed5e6cb866145b51db34
Jlopez127 <jmlm981127@gmail.com>
2026-04-21 16:20:26 -0500

    Advertencia en error sin costo 5

--- Manifiestos_astrid.py
+++ Manifiestos_astrid.py
@@ -268,7 +268,7 @@ if run:
     # EXCEPTO guías permitidas
     # -----------------------------
     
-    GUIAS_EXCLUIDAS_ALERTA = {"85674", "8646","82760","83685"}
+    GUIAS_EXCLUIDAS_ALERTA = {"85674", "8646","82760","83685","84147"}
     
     # normalizar guia como texto limpio
     df_final["guia"] = _clean_str_series(df_final["guia"])
